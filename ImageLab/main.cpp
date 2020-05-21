0dfc36065fdb261f03b178071f49020944cd0d4d
stormBMO <andrey.chet76@gmail.com>
2020-05-21 10:38:18 +0300

    added 2 funcs to main.cpp

--- ImageLab/main.cpp
+++ ImageLab/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main(){
      vector<vector<int>> mainMatrix; //main image (matrix of pixels [0 - 255])
-     
+     MatrixInput(mainMatrix);
+     MatrixOutput(mainMatrix);
      return 0;
 }
