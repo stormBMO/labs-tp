41a8d27deb8490ff878484f15579d587f433f000
stormbmo <andrey.chetvertkov@mdinc.ru>
2020-05-22 20:30:21 +0300

    WIP

--- ImageLab/solve.cpp
+++ ImageLab/solve.cpp
@@ -11,13 +11,13 @@ int DFS(const int& start, vector<vector<pair<int, int>>>& matrix, const int& col
     stack<pair<int,int>> s;
     s.push({start, 0});
     while (!s.empty()) {
-        pair<int, int> v = s.top();
+        int v = s.top().first;
         s.pop();
-        for (int i = 0; i < matrix[v.first].size(); ++i){
-            if (matrix[v.first][i].second == 0 && matrix[v.first][i].first == start){
-                s.push(matrix[v.first][i]);
-                matrix[v.first][i].second = 1;
-                matrix[v.first][i].first = color;
+        for (int i = 0; i < matrix[v].size()*matrix[v].size(); ++i){
+            if (matrix[v][i].second == 0){
+                s.push(matrix[v][i]);
+                matrix[v][i].second = 1;
+                matrix[v][i].first = color;
             }
         }
     }
